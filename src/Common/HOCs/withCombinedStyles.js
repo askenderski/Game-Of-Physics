13741a699be716d4bb7955646c15eb6868cc4dd9
skenderskiantonio <skenderskia@gmail.com>
2021-04-21 11:03:05 +0300

    Removed combined styles
    Instead of combining styles in HOCs, they will be combined using SASS.

--- src/Common/HOCs/withCombinedStyles.js
+++
@@ -1,9 +0,0 @@
-import {combineClassNames} from "../Utilities/components";
-
-export default function withCombinedStyles(WrappedComponent, defaultClassName) {
-    return ({className: newClassName, ...rest}) => {
-        const className = combineClassNames(newClassName, defaultClassName);
-
-        return <WrappedComponent className={className} {...rest} />;
-    };
-};
