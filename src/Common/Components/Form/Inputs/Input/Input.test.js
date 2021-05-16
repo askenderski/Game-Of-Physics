536307f7e227834043a843591dc5d703044cd9b6
askenderski <skenderskia@gmail.com>
2021-05-16 15:38:00 +0300

    Refactored Input tests to now include module tests
    Module tests are the tests that will be used when integration with the Input component.

--- src/Common/Components/Form/Inputs/Input/Input.test.js
+++ src/Common/Components/Form/Inputs/Input/Input.test.js
@@ -1,43 +1,10 @@
-import getInputPropsFromFormData from "./getInputPropsFromFormData/getInputPropsFromFormData";
-import executeTests from "./InputTests";
-import InputTestsDataAndUtilities from "./InputTestsDataAndUtilities";
-
-jest.mock("./getInputPropsFromFormData/getInputPropsFromFormData", () => jest.fn());
-
-function createFormWithInputComponentUnit({propsToPassManually, formProps}) {
-    const name = "someName";
-    const formValuesToBePassed = {};
-    const inputType = InputTestsDataAndUtilities.inputTypeDifferentFromDefault;
-
-    getInputPropsFromFormData.mockImplementationOnce((formData, curName, curInputType) => {
-        if (formData === formValuesToBePassed && name === curName && curInputType === inputType) {
-            return formProps;
-        }
-
-        return {};
-    });
-
-    return createFormWithInputComponentIntegration({propsToPassManually,
-        formProps: formValuesToBePassed});
-}
-
-function createFormWithInputComponentIntegration({propsToPassManually, formProps}) {
-    const name = "someName";
-    const inputType = InputTestsDataAndUtilities.inputTypeDifferentFromDefault;
-
-    const input = InputTestsDataAndUtilities.renderInputWithPropGetter(
-        {name, inputType: InputTestsDataAndUtilities.inputTypeDifferentFromDefault, ...propsToPassManually},
-        formProps
-    );
-    const inputElement = InputTestsDataAndUtilities.getInputElementByWrapperWithProps(input, inputType);
-
-    return {getProp: propName => InputTestsDataAndUtilities.getProp(inputElement, propName)};
-}
+import {executeIntegrationTests, executeUnitTests} from "./InputModuleTests";
 
 describe("unit tests", () => {
-    executeTests({createFormWithInputComponent: createFormWithInputComponentUnit});
+    executeUnitTests();
 });
 
+//This tests the integration of the Input component with the getInputPropsFromFormData function
 describe("integration tests", () => {
-    executeTests({createFormWithInputComponent: createFormWithInputComponentIntegration});
+    executeIntegrationTests();
 });
---
+++ src/Common/Components/Form/Inputs/Input/InputModuleTests.js
@@ -0,0 +1,46 @@
+import InputTestsDataAndUtilities from "./InputTestsDataAndUtilities";
+import getInputPropsFromFormData from "./getInputPropsFromFormData/getInputPropsFromFormData";
+import executeTests from "./InputTests";
+
+jest.mock("./getInputPropsFromFormData/getInputPropsFromFormData", () => jest.fn());
+
+function createFormWithInputComponentUnit({propsToPassManually, formProps}) {
+    const name = "someName";
+    const formValuesToBePassed = {};
+    const inputType = InputTestsDataAndUtilities.inputTypeDifferentFromDefault;
+
+    getInputPropsFromFormData.mockImplementationOnce((formData, curName, curInputType) => {
+        if (formData === formValuesToBePassed && name === curName && curInputType === inputType) {
+            return formProps;
+        }
+
+        return {};
+    });
+
+    return createFormWithInputComponentIntegration({propsToPassManually,
+        formProps: formValuesToBePassed});
+}
+
+function createFormWithInputComponentIntegration({propsToPassManually, formProps}) {
+    const name = "someName";
+    const inputType = InputTestsDataAndUtilities.inputTypeDifferentFromDefault;
+
+    const input = InputTestsDataAndUtilities.renderInputWithPropGetter(
+        {name, inputType: InputTestsDataAndUtilities.inputTypeDifferentFromDefault, ...propsToPassManually},
+        formProps
+    );
+    const inputElement = InputTestsDataAndUtilities.getInputElementByWrapperWithProps(input, inputType);
+
+    return {getProp: propName => InputTestsDataAndUtilities.getProp(inputElement, propName)};
+}
+
+export const executeUnitTests =
+    executeTests.bind(executeTests, {createFormWithInputComponent: createFormWithInputComponentUnit});
+
+export const executeIntegrationTests =
+    executeTests.bind(executeTests, {createFormWithInputComponent: createFormWithInputComponentIntegration});
+
+const executeModuleTests =
+    executeTests.bind(executeTests, {createFormWithInputComponent: createFormWithInputComponentIntegration});
+
+export default executeModuleTests;
