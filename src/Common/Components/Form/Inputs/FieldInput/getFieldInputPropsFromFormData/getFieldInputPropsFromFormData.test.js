d2604d9e21951b46af5044278fb659310ac04a2c
askenderski <skenderskia@gmail.com>
2021-05-25 17:56:46 +0300

    Refactored getFieldInputPropsFromFormData tests not to directly rely on getFieldInputPropsFromFormData

--- src/Common/Components/Form/Inputs/FieldInput/getFieldInputPropsFromFormData/getFieldInputPropsFromFormData.test.js
+++ src/Common/Components/Form/Inputs/FieldInput/getFieldInputPropsFromFormData/getFieldInputPropsFromFormData.test.js
@@ -1,5 +1,16 @@
 import executeTests from "./getFieldInputPropsFromFormDataTests";
+import getFieldInputPropsFromFormData from "./getFieldInputPropsFromFormData";
+
+function getPropExtractor(formProps, name) {
+    const res = getFieldInputPropsFromFormData(formProps, name);
+
+    return {
+        executeOnChangeByValue(value) {
+            res.onChange({target: {value}});
+        }
+    };
+}
 
 describe("unit tests", () => {
-    executeTests();
+    executeTests({getPropExtractor});
 });
--- src/Common/Components/Form/Inputs/FieldInput/getFieldInputPropsFromFormData/getFieldInputPropsFromFormDataTests.js
+++ src/Common/Components/Form/Inputs/FieldInput/getFieldInputPropsFromFormData/getFieldInputPropsFromFormDataTests.js
@@ -1,12 +1,10 @@
-import getFieldInputPropsFromFormData from "./getFieldInputPropsFromFormData";
-
-const executeTests = () => {
+const executeTests = ({getPropExtractor}) => {
     test("getFieldInputPropsFromFormDataTests gets correct onChange", () => {
         const fieldName = "name of field";
         let hasInvokedSetFieldValueWithCorrectParams = false;
         const valueToCallOnChangeWith = "some value";
 
-        const fieldInputProps = getFieldInputPropsFromFormData({
+        const propExtractor = getPropExtractor({
             setFieldValue: (name, value) => {
                 if (name === fieldName && value === valueToCallOnChangeWith) {
                     hasInvokedSetFieldValueWithCorrectParams = true;
@@ -14,7 +12,7 @@ const executeTests = () => {
             }
         }, fieldName);
 
-        fieldInputProps.onChange({target: {value: valueToCallOnChangeWith}});
+        propExtractor.executeOnChangeByValue(valueToCallOnChangeWith);
 
         expect(hasInvokedSetFieldValueWithCorrectParams).toBe(true);
     });
